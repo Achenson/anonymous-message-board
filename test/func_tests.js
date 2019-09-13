2a9064c05ebdcab5fef1ca01b2f4d8bdb7b31f25
achenson <45999022+Achenson@users.noreply.github.com>
2019-09-13 14:40:56 +0200

    test replies get & post

--- test/func_tests.js
+++ test/func_tests.js
@@ -13,6 +13,7 @@ describe('API ROUTING FOR /api/threads/:board', function() {
         .request(server)
         .post('/api/threads/:board')
         .send({
+          
           board: 'general',
           text: 'mocha testing',
           delete_password: 'mocha testing password'
@@ -47,6 +48,8 @@ describe('API ROUTING FOR /api/threads/:board', function() {
         assert.property(res.body[0], 'text');
         assert.property(res.body[0], 'replies'); 
         assert.property(res.body[0], 'board');   
+
+        //dodać replies. properties tests!
 
         assert.isAtMost(res.body[0].replies.length, 3);
 
@@ -99,17 +102,75 @@ describe('API ROUTING FOR /api/threads/:board', function() {
   
 });
 
-/*
+
 describe('API ROUTING FOR /api/replies/:board', function() {
   
+  
   describe('POST', function() {
+    it('posting reply', function(done) {
+      chai
+        .request(server)
+        .post('/api/replies/:board')
+        .send({
+          board: 'new board',
+          thread_id: '5d7b7f46a41a551cb8309851',
+          text: 'mocha reply',
+          delete_password: 'mocha reply'
+      
+         
+        })
+        .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "text/html");
+
+        done()
+        })
+    })
+
     
   });
   
+  //works
   describe('GET', function() {
+    it('board: getting threads with all replies', function(done) {
+      chai
+        .request(server)
+        .get('/api/replies/new board?thread_id=5d7a4d94acb7762194502de0')
+        .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        
+
+        assert.notProperty(res.body.replies[0], 'delete_password');
+        assert.notProperty(res.body.replies[0], 'reported');
+
+        assert.property(res.body.replies[0], '_id');
+        assert.property(res.body.replies[0], 'created_on');
+       
+        assert.property(res.body.replies[0], 'text');
+           
+
+        
+
+
+
+
+
+
+
+
+
+
+        done()
+        })
+    })
+
+
+
     
   });
   
+  /*
   describe('PUT', function() {
     
   });
@@ -117,6 +178,5 @@ describe('API ROUTING FOR /api/replies/:board', function() {
   describe('DELETE', function() {
     
   });
-  
+  */
 });
-*/
